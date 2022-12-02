acc1c69fb6e5d15796387d4b8ad2d4b7816dc117
LordAngel11 <juan.lopez@cimat.mx>
2022-12-02 14:19:56 -0600

    -absolute path

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -2,21 +2,17 @@ import numpy as np
 import plotly as pl
 from zipfile import ZipFile
 import pandas as pd
-import os 
 
 print("Proyecto del datathon")
 
 #Primero vamos a leer los archivos de zipfile
 
-
-
-S1file = "/home/lordangel11/Documentos/Maalik/Data/zips/datos_PDN_all_22_09_2022.zip"
+S1file = "Maalik/Data/zips/datos_PDN_all_22_09_2022.zip"
 #S2file = "Data/S2datos_PDN_all_22_09_2022.zip"
 #S3file = "Data/S3datos_PDN_all_22_09_2022.zip"
 
 with ZipFile(S1file, 'r') as zip:
     
-   zip.printdir()
-   print("Ya leyo todos los archivos del S1")
-    
+    zip.printdir()
+    print("Ya leyo todos los archivos del S1")
     
