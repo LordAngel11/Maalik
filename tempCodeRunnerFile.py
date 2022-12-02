83791a4949c9314595d98d7d3270e8042d0a4eac
JossPL <josue.perez@cimat.mx>
2022-12-02 15:12:42 -0600

    Extraction from zip

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,18 +1,21 @@
 import numpy as np
-import plotly as pl
+#import plotly as pl
 from zipfile import ZipFile
-import pandas as pd
+#import pandas as pd
 
 print("Proyecto del datathon")
 
 #Primero vamos a leer los archivos de zipfile
 
-S1file = "Maalik/Data/zips/datos_PDN_all_22_09_2022.zip"
+S1file = "C:/Users/jossa/OneDrive - Universidad de Guanajuato\Documentos/Josue/Demat/Hackathon/Datathon 2022/datos_PDN_all_22_09_2022.zip"
 #S2file = "Data/S2datos_PDN_all_22_09_2022.zip"
 #S3file = "Data/S3datos_PDN_all_22_09_2022.zip"
 
 with ZipFile(S1file, 'r') as zip:
     
+    
+    print(zip.infolist()[0].filename)
+
     zip.printdir()
     print("Ya leyo todos los archivos del S1")
     
