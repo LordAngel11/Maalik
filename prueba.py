146d5dc020a2fe64aca1d184e93b98ac5fa67413
LordAngel11 <juan.lopez@cimat.mx>
2022-12-02 13:57:53 -0600

    -update chido

--- prueba.py
+++ prueba.py
@@ -1,1 +1,18 @@
-print("si jalo")
+import numpy as np
+import plotly as pl
+from zipfile import ZipFile
+import pandas as pd
+
+print("Proyecto del datathon")
+
+#Primero vamos a leer los archivos de zipfile
+
+S1file = "Data/S1datos_PDN_all_22_09_2022.zip"
+S2file = "Data/S2datos_PDN_all_22_09_2022.zip"
+S3file = "Data/S3datos_PDN_all_22_09_2022.zip"
+
+with ZipFile(S1file, 'r') as zip:
+    
+    zip.printdir()
+    print("Ya leyo todos los archivos del S1")
+    
